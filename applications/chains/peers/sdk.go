a0030b74dd2992070bf00ef4d4a7669017fb29cc
Steve Rodrigue <stev.rodr@gmail.com>
2023-03-12 11:46:07 -0400

    Added the peer eventsd

--- applications/chains/peers/sdk.go
+++ applications/chains/peers/sdk.go
@@ -4,6 +4,12 @@ import (
 	"net/url"
 )
 
+// EnterOnConnectFn represents the enter's onConnect func
+type EnterOnConnectFn func(peer *url.URL) error
+
+// ExitOnConnectFn represents the exit's onConnect func
+type ExitOnConnectFn func(peer *url.URL) error
+
 // Application represents the peers application
 type Application interface {
 	List() ([]*url.URL, error)
