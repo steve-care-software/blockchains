c90cce37ae7620df156b2c834d3c63b02651a992
Steve Rodrigue <stev.rodr@gmail.com>
2023-03-12 11:41:59 -0400

    Added the identity's creation time

--- domain/identities/sdk.go
+++ domain/identities/sdk.go
@@ -1,11 +1,14 @@
 package identities
 
+import "time"
+
 // Builder represents an identity builder
 type Builder interface {
 	Create() Builder
 	WithName(name string) Builder
 	WithPrivate(private []byte) Builder
 	WithPublic(public []byte) Builder
+	CreatedOn(createdOn time.Time) Builder
 	Now() (Identity, error)
 }
 
@@ -14,4 +17,5 @@ type Identity interface {
 	Name() string
 	Private() []byte
 	Public() []byte
+	CreatedOn() time.Time
 }
